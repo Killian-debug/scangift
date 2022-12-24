617c542459c33f52b8c93841ac4fb12d67ad7bbf
Killian-debug <kitsunkilli@gmail.com>
2022-12-24 19:33:34 +0100

    new branch

--- src/views/Home.js
+++ src/views/Home.js
@@ -13,7 +13,7 @@ const Home = () => {
         <div>
             <div className="card bg-transparent text-center border-0">
                 <div className="card-header bg-transparent border-0">
-                    <h3 className='title-s-1 my-2' >Scanner pour gagner ! </h3> 
+                    <h3 className='title-s-1 my-2' >Scanner pour gagner !</h3> 
                 </div>
                 <div className="card-body d-flex flex-column justify-content-center align-items-center">
                     <div className="scangif m-2 text-center">
