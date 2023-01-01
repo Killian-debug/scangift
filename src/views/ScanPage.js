2a07715accead0b0c81239145d208aae22896bb3
Killian-debug <kitsunkilli@gmail.com>
2023-01-01 03:36:16 +0100

    popup de urlscanned2

--- src/views/ScanPage.js
+++ src/views/ScanPage.js
@@ -22,7 +22,7 @@ const ScanPage = () => {
   const form = FormUrl;
 
   const Url_Valide = (UrlTest) => {
-    var regexp = /[(http(s)?):(www.)?a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_+.~#?&//=]*)/g;
+    var regexp = /[(http(s)?):(www.)?a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_+.~#?&//=]*)/ig;
 
     // if (regexp.test(UrlTest)){
     //   alert ('Mon URL est valide');
